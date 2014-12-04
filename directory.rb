2bd3672f1f88e2fef08a22ef841a607867b3bd05
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-04 00:03:37 +0000

    first version of dir.rb

---
+++ directory.rb
@@ -0,0 +1,12 @@
+puts "The students of my cohort at Makers Academy"
+puts "--------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex De Large"
+puts "Teh Alien"
+puts "Termiantor"
+puts "Fredy Kruger"
+puts "The Joker"
+
