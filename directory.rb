bde63391295ec502fb40317d61306925db6e123c
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-05 23:50:05 +0000

    getting rid off put's

--- directory.rb
+++ directory.rb
@@ -16,22 +16,22 @@ students = [
 
 def print_names(students)
 	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort"
+		print "#{student[:name]} (#{student[:cohort]} cohort)\n"
 	end
 end
 
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "--------------"
+  print "The students of my cohort at Makers Academy\n"
+  print "--------------\n"
 end
 
 def print_footer(names)
-  print "Overall, we have #{names.length} great students"	
+  print "Overall, we have #{names.length} great students\n"	
 end
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\n"
+	print "To finish, just hit return twice\n"
 	#create an empty array
 	students = []
 	#get first name
@@ -40,7 +40,7 @@ def input_students
 	while !name.empty? do
 		#add the student hsah 
 		students << {:name => name, :cohort => :novmeber}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		#get another name from the user
 		name = gets.chomp
 	end
