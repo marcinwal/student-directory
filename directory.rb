5d30f99a81f8471a2abb07d048cc1e30efe18665
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-05 23:42:04 +0000

    manual input of students

--- directory.rb
+++ directory.rb
@@ -29,8 +29,27 @@ def print_footer(names)
   print "Overall, we have #{names.length} great students"	
 end
 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	#create an empty array
+	students = []
+	#get first name
+	name = gets.chomp
+	#while the name is not empty repea this code
+	while !name.empty? do
+		#add the student hsah 
+		students << {:name => name, :cohort => :novmeber}
+		puts "Now we have #{students.length} students"
+		#get another name from the user
+		name = gets.chomp
+	end
+	#return the arry of students
+	students
+end
 
 
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
