25ea315c2601e8dcab7b59856396813f00d257e1
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-07 20:48:22 +0000

    adding menu and global @students

--- directory.rb
+++ directory.rb
@@ -14,12 +14,12 @@ students = [
  {:name =>"The Joker", :cohort => :september,:hobbies => "sport", :birth_country => "usa", :eye_color => :brown}
 ]
 
-months = [:september, :december ,:march ,:june, :november]	
 
-def print_names(students,pattern=/A(.)+/,max_name_length = 12)
-	students.each_with_index do |student,index|
-		print "#{index}. #{student[:name]} (#{student[:cohort]} cohort)\n" if (student[:name]=~pattern) && 
-																			  (student[:name].length < max_name_length)
+
+def print_names(pattern=/(.)*/,max_name_length = 120)
+	@students.each_with_index do |student,index|
+		print "#{index}. #{student[:name]} (#{student[:cohort]} cohort)\n" #if (student[:name]=~pattern) && 
+																		   #	  (student[:name].length < max_name_length)
 	end
 end
 
@@ -34,7 +34,7 @@ def print_names_by_cohort(students,months)
 
 end
 
-def print_names_with_while(students,pattern=/A(.)+/,max_name_length = 12)
+def print_names_with_while(students,pattern=/(.)*/,max_name_length = 120)
 	studs = students.length 
 	i = 0 
 	while i < studs
@@ -49,58 +49,102 @@ def print_header
   print "--------------\n"
 end
 
-def print_footer(names)
+def print_footer()
   plural = 1 #how many s's should be added
-  plural = 0 if names.length == 1	
-  print "Overall, we have #{names.length} great student" + 's' * plural 	
+  plural = 0 if @students.length == 1	
+  print "Overall, we have #{@students.length} great student" + 's' * plural 	
 end
 
-def input_details(name,months)
+def input_details(name)
 
-	print "Please enter #{name} cohort out of #{months} "
+	print "Please enter #{name} cohort out of #{@months} "
 	coh = gets.chomp.to_sym
 
-	puts "selection not in options #{months} set to defualt" unless months.include?(coh)
-    coh = "november" unless months.include?(coh)
+	puts "selection not in options #{@months} set to defualt" unless @months.include?(coh)
+    coh = "november" unless @months.include?(coh)
 
 	print "Please enter #{name} hobbies "
 	hobbies = gets.chomp
 	print "Please enter #{name} conutry of birth "
 	country = gets.chomp
 	print "Please enter #{name} eye color "
-	eyes = gets.chomp
+	eyes = gets.tr('\n','') # substitute of chomp
+	#print eyes
 	return hobbies,country,eyes,coh
 end
 
 
-def input_students(months)
+def input_students()
 	print "Please enter the names of the students\n"
 	print "To finish, just hit return twice\n"
 	#create an empty array
-	students = []
+	#students = []
 	#get first name
 	name = gets.chomp
 	#while the name is not empty repea this code
 	while !name.empty? do
 		#add the student hash
-		hobby,country,eye,coh = input_details(name,months)
+		hobby,country,eye,coh = input_details(name)
 		#puts  "#{hobby} ,#{country}, #{eye}"
-		students << {:name => name, :cohort => coh, :hobbies => hobby, :birth_country => country, :eye_color => eye.to_sym }
-		print "Now we have #{students.length} students\n"
+		@students << {:name => name, :cohort => coh, :hobbies => hobby, :birth_country => country, :eye_color => eye.to_sym }
+		print "Now we have #{@students.length} students\n"
 		#get another name from the user
 		name = gets.chomp
 	end
 	#return the arry of students
-	students
+	#students
 end
 
-pattern = /A(.)+/
-students = input_students(months)
-print_header
-print_names(students,pattern)
-print_footer(students)
-print_names_with_while(students,pattern)
+def print_menu
+	puts "Menu"
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end	
+
+def show_students()
+		print_header
+		print_names()
+		print_footer()
+end
+
+def process(selection)
+	case selection
+	  when "1"
+		input_students()
+	  when "2"
+		show_students()
+	  when "9"
+		exit
+	  else
+		puts "I do not know what you meant, try again"
+	end
+end	
+
+def interactive_menu
+  @months = [:september, :december ,:march ,:june, :november]		
+  @students = []
+  loop do
+  	print_menu	
+	process(gets.chomp)
+  end
+end
+
+def old_main_run
+  pattern = /A(.)+/
+  students = input_students(months)
+  print_header
+  print_names(students,pattern)
+  print_footer(students)
+  print_names_with_while(students,pattern)
 
 
-print print_names_by_cohort(students,months)
+  print print_names_by_cohort(students,months)
+end
+
+interactive_menu
+
+
+
+
 
