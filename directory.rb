1178eb3c1e2c644d46a43d437ef30ff04acede76
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-07 21:04:56 +0000

    adding loading option

--- directory.rb
+++ directory.rb
@@ -74,6 +74,16 @@ def input_details(name)
 	return hobbies,country,eyes,coh
 end
 
+def load_students
+	file = File.open("students.csv","r")
+	file.readlines.each do |line|
+		name,cohort = line.chomp.split(",")
+		@students << {:name => name , :cohort => cohort.to_sym}
+	end
+	file.close
+end
+
+
 def save_students
 	file = File.open("students.csv","w")
 	@students.each do |student|
@@ -110,6 +120,7 @@ def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
 	puts "3. Save the list to students.csv"
+	puts "4. Load the list from students.csv"
 	puts "9. Exit"
 end	
 
@@ -127,6 +138,8 @@ def process(selection)
 		show_students
 	  when "3"
 	    save_students	
+	  when "4"
+	  	load_students  
 	  when "9"
 		exit
 	  else
